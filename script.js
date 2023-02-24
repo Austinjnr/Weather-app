5e51176aa2c3c594bc65cb0558a5f492d658ed06
austinjnr <austinobonyo@gmail.com>
2023-02-24 23:43:57 +0300

    adds conditional statements

--- script.js
+++ script.js
@@ -6,5 +6,56 @@ const error404= document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
     const APIKey ='728b0ee6df5687559812bd3169ad77b7';
-    const city = document.querySelector()
+    const city = document.querySelector('.searcg-box input').value;
+
+    if(city === '')
+         return;
+
+         fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}&').then(response => response.json()).then(json => {
+
+         if (json.cod === '404'){
+            container.style.height = '404px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add ('fadeIn');
+            return;
+         }
+
+         error404.style.display = 'none';
+         error404.classList.remove ('fadeIn');
+
+         const image = document.querySelector ('.weather-box img');
+         const temperature = document.querySelector ('.weather-box temperature');
+         const description = document.querySelector ('.weather-box description');
+         const humidity = document.querySelector ('.weather-box humidity span');
+         const wind = document.querySelector ('.weather-box wind span');
+
+         switch (json.weather[0].main){
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+            
+             case 'Rain':
+                image.src = 'images/clear.png';
+                break;
+            
+             case 'Sonw':
+                image.src = 'images/clear.png';
+                break;
+
+            case 'Clouds':
+                image.src = 'images/clear.png';
+                break;
+
+             case 'Haze':
+                image.src = 'images/clear.png';
+                 break;
+
+            defualt:
+                image.src = ''; 
+            }
+
+            temperature.innerHTML = 
+         })
 })
